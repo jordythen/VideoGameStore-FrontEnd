a372d4f6eae6a00ae2652390e80c369c3753d3ad
Raul Gonzalez <rgonz166@gmail.com>
2020-07-16 22:55:10 -0700

    Add: login invalid login 2 options

--- VideoGameStore/src/app/login/login.component.ts
+++ VideoGameStore/src/app/login/login.component.ts
@@ -8,43 +8,53 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
   loggedUser: User;
   username: string;
   password: string;
   statusCode: number;
   registerFlag: boolean;
+  invalidLogin: boolean;
+  passwordView: boolean;
+  passInputType: string;
   public displayLoginLoader: Observable<boolean> = this.userService.isLoading();
 
-  constructor(private userService: UserService, private homeComp: HomeComponent, private mainNavComp: MainNavComponent) { }
+  constructor(
+    private userService: UserService,
+    private homeComp: HomeComponent,
+    private mainNavComp: MainNavComponent
+  ) {}
 
   ngOnInit(): void {
     this.username = '';
     this.password = '';
+    this.passwordView = false;
+    this.passInputType = 'password';
+    this.invalidLogin = false;
     this.registerFlag = this.homeComp.registerFlag;
   }
 
   login() {
-    this.userService.loginUser(this.username, this.password).subscribe(
-      resp => {
+    this.userService
+      .loginUser(this.username, this.password)
+      .subscribe((resp) => {
         this.loggedUser = resp.body;
         this.statusCode = resp.status;
         if (this.statusCode !== 200) {
           this.userService.loader.next(false);
-          alert(' WRONG USERNAME/PASSWORD ');
-        } else { // its good to go
-          
+          this.invalidLogin = true;
+        } else {
+          // its good to go
+
           this.userService.loader.next(false);
           this.mainNavComp.loggedUser = this.loggedUser;
           this.mainNavComp.account = `${this.loggedUser.firstName} ${this.loggedUser.lastName}`;
           this.homeComp.loggedInGrid();
           this.homeComp.loggedUser = this.loggedUser;
         }
-      }
-    );
+      });
   }
 
   toRegister() {
@@ -52,4 +62,13 @@ export class LoginComponent implements OnInit {
     this.homeComp.registerFlag = true;
   }
 
+  togglePassView() {
+    console.log('toggled');
+    this.passwordView = !this.passwordView;
+    if (this.passwordView) {
+      this.passInputType = 'text';
+    } else {
+      this.passInputType = 'password';
+    }
+  }
 }
